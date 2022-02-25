d77c884807c1a6237fd3af2bbcc78dccd9769710
RobXela <78446731+RobXela@users.noreply.github.com>
2022-02-25 16:51:08 +0300

    Initialized App structure & Applied Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import { SideBar } from "./components/SideBar/SideBar";
+import { Header } from "./components/Header/Header";
+import { MyPage } from "./components/MyPage/MyPage";
+import { MainPage } from "./components/MainPage/MainPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      
+      <Header />
+      <SideBar />
+      <div className="content">
+        <Routes>
+          <Route path="main" element={<MainPage />}></Route>
+          <Route path="my-page" element={<MyPage />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
