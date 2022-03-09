c9e3cd3d76d50b40623cf2bcb95414ec6881bee6
RobXela <78446731+RobXela@users.noreply.github.com>
2022-03-09 22:08:06 +0300

    Create Main Page component

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import style from "./MainPage.module.css";
+
+
+export const MainPage = () => {
+  return (
+    <div className={style.main}>
+      <div>#TAGS</div>
+      <div>Sort</div>
+      <div>tab with reviews</div>
+    </div>
+  );
+};
