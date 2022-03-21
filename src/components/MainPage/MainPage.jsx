e144e73d311f52142cce895b6f92c0ccf530e510
RobXela <78446731+RobXela@users.noreply.github.com>
2022-03-22 01:01:22 +0300

    Added table to MainPage component

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,12 +1,26 @@
+import { useMemo } from "react";
+import { Table } from "../Table/Table";
 import style from "./MainPage.module.css";
 
-
 export const MainPage = () => {
+  const reviews = require("../../reviews.json");
+
+  const review = reviews[0];
+
+  const columns = useMemo(() => [
+    { Header: "Title", accessor: "title" },
+    { Header: "Author", accessor: "author" },
+    { Header: "Group", accessor: "group" },
+    { Header: "Tags", accessor: "tags" },
+  ]);
+  const data = useMemo(() => reviews, []);
+
   return (
     <div className={style.main}>
-      <div>#TAGS</div>
-      <div>Sort</div>
-      <div>tab with reviews</div>
+      <div>{review.tags.map((tag) => `#${tag}`)}</div>
+      <div>
+        <Table columns={columns} data={reviews}></Table>
+      </div>
     </div>
   );
 };
