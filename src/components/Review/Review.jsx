c253355b531499807da0960ed9abd9539fcc2e79
RobXela <78446731+RobXela@users.noreply.github.com>
2022-03-21 21:34:32 +0300

    Added rating visual in Review component

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,9 +1,16 @@
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+import styles from "./Review.module.css";
+import starIcon from "../../icons/star.svg";
+
 export const Review = () => {
   const data = require("../../reviews.json");
 
   const review = data[0];
-  const rating = review.rating
-  const averagedRating = (rating.reduce((a, b) => a + b) / rating.length).toFixed(1)
+  const rating = review.rating;
+  const averagedRating = (
+    rating.reduce((a, b) => a + b) / rating.length
+  ).toFixed(1);
 
   return (
     <div>
@@ -11,7 +18,30 @@ export const Review = () => {
       <div>{review.group}</div>
       <div>{review.tags.map((tag) => `#${tag}`)}</div>
       <div>{review.text}</div>
-      <div>Оценки: {review.rating} Рейтинг: {averagedRating}</div>
+      <div className={styles.rating}>
+        <img src={starIcon} alt=""></img>
+        <div>{averagedRating}</div>
+      </div>
+      <div>
+        <div>Оцените обзор</div>
+        <ButtonGroup>
+          <Button variant="outline-primary">
+            <img src={starIcon} alt=""></img>
+          </Button>
+          <Button variant="outline-primary">
+            <img src={starIcon} alt=""></img>
+          </Button>
+          <Button variant="outline-primary">
+            <img src={starIcon} alt=""></img>
+          </Button>
+          <Button variant="outline-primary">
+            <img src={starIcon} alt=""></img>
+          </Button>
+          <Button variant="outline-primary">
+            <img src={starIcon} alt=""></img>
+          </Button>
+        </ButtonGroup>
+      </div>
     </div>
   );
 };
